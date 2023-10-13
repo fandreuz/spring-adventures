7eeffb7156c0cbe5e8a724879e5ee354d5968c66
Francesco Andreuzzi <francesco.andreuzzi@cern.ch>
2023-10-13 12:29:34 +0200

    bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("io.freefair.lombok") version "8.0.1"
+    id("io.freefair.lombok") version "8.3"
     id("com.diffplug.spotless") version "6.19.0"
 }
 
@@ -12,12 +12,16 @@ dependencies {
     implementation("org.slf4j:slf4j-api:PRO")
     implementation("ch.qos.logback:logback-classic:PRO")
     implementation("org.springframework.boot:spring-boot-starter-web:PRO")
+    implementation("org.springframework:spring-core:PRO")
+    implementation("org.springframework:spring-beans:PRO")
+    implementation("org.springframework:spring-context:PRO")
+    implementation("org.springframework:spring-expression:PRO")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test:PRO")
-    api("org.junit.jupiter:junit-jupiter:PRO")
-    api("org.junit-pioneer:junit-pioneer:PRO")
-    api("org.assertj:assertj-core:PRO")
-    api("org.mockito:mockito-core:PRO")
+    testImplementation("org.junit.jupiter:junit-jupiter:PRO")
+    testImplementation("org.junit-pioneer:junit-pioneer:PRO")
+    testImplementation("org.assertj:assertj-core:PRO")
+    testImplementation("org.mockito:mockito-core:PRO")
 }
 
 tasks.test {
