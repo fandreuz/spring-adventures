13031fa828675974f4566c34707c841714dc8496
Francesco Andreuzzi <francesco.andreuzzi@cern.ch>
2024-09-11 00:02:29 +0200

    dont use pro

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,33 @@
 plugins {
     id("io.freefair.lombok") version "8.3"
     id("com.diffplug.spotless") version "6.19.0"
+    java
+}
+
+repositories {
+    mavenCentral()
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:PRO")
-    implementation("ch.qos.logback:logback-classic:PRO")
-    implementation("org.springframework.boot:spring-boot-starter-web:PRO")
-    implementation("org.springframework:spring-core:PRO")
-    implementation("org.springframework:spring-beans:PRO")
-    implementation("org.springframework:spring-context:PRO")
-    implementation("org.springframework:spring-expression:PRO")
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    implementation("ch.qos.logback:logback-classic:1.5.8")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.7.16")
+    implementation("org.springframework:spring-core:5.3.30")
+    implementation("org.springframework:spring-beans:5.3.30")
+    implementation("org.springframework:spring-context:5.3.30")
+    implementation("org.springframework:spring-expression:5.3.30")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test:PRO")
-    testImplementation("org.junit.jupiter:junit-jupiter:PRO")
-    testImplementation("org.junit-pioneer:junit-pioneer:PRO")
-    testImplementation("org.assertj:assertj-core:PRO")
-    testImplementation("org.mockito:mockito-core:PRO")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.16")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
+    testImplementation("org.junit-pioneer:junit-pioneer:2.1.0")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("org.mockito:mockito-core:4.5.1")
 }
 
 tasks.test {
